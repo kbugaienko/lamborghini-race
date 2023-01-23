61adb53d8b2166efe3b8552aaae08046211f50f9
Khrystyna <kr.bugaienko@gmail.com>
2023-01-23 22:04:05 +0100

    Create blog section
    * Create new section blog
    * Add two images for a slider
    * Add two arrows for slider
    * Add js files for Swiper to create slider
    * Add styles

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 20,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
+  },
+});
+  
