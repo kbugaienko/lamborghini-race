1972f85ccd2880c19f36cf7c8f45532cabfaf83c
Khrystyna <kr.bugaienko@gmail.com>
2023-01-26 17:34:13 +0100

    Adaptive website layout for header&promo sections
    * Add styles
    * Create burger-menu for mobile versions
    * Add JS for clickable burger-menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const menuBtn = document.querySelector('.menu-btn') ;
+const menuMobile = document.querySelector('.menu-mobile') ;
+
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu--open');
+});
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
