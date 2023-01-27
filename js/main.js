145dd643d718ed468f2c490ac56c5ede6fc7ec96
Khrystyna <kr.bugaienko@gmail.com>
2023-01-27 12:42:00 +0100

    Adaptive website layout for blog section
    * Add styles
    * In JS-file modify settings for size less than 768px changed slider display to 1 image

--- js/main.js
+++ js/main.js
@@ -16,6 +16,15 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-right',
     prevEl: '.swiper-button-left',
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    }
+  }
 });
 
 let map;
